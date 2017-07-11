54f47e07cddcc7c6b5185dbea65948f14cbe2bdc
gitter007 <erkashine@yahoo.com>
2017-07-11 00:39:12 -0500

    Exercise2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public counter = 5;
   title = 'Lab11';
+
+  public outputData;
+
+  showOutputData(data){
+    this.outputData = data;
+  }  
 }
--- src/app/counter.component.ts
+++ src/app/counter.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   template: ` 
     <h3>Counter demo:</h3>
-    <div id = "counter">
-    <button (click)="decrement()">-</button>
-         {{counterValue}}
-    <button (click)="increment()">+</button>
+    <div id = "counterDiv">
+      <button (click)="decrement()">-</button>
+          {{counterValue}}
+      <button (click)="increment()">+</button>
     </div>
+    <p>Output "data Obj" to Parent: <button (click)="emitMessage()">Click to Output!</button></p>
+
+
      `,
-  styles: [` #counter { 
+  styles: [` #counterDiv { 
     font-weight:bold; 
     border: 1px solid gray;
     display: inline;
     padding:5px }`],
 })
 export class CounterComponent{
-  private counterValue;
+  @Input() private counterValue;
   constructor() { 
     this.counterValue =0 ;
   }
 
   decrement(){
-    this.counterValue = this.counterValue-1;
+    this.counterValue = parseInt(this.counterValue)-1;
+    this.counterChange.emit(this.counterValue);
   }
 
   increment(){
-    this.counterValue = this.counterValue+1;
+    this.counterValue = parseInt(this.counterValue)+1;
+    this.counterChange.emit(this.counterValue);
+  }
+
+  @Output() counterChange = new EventEmitter();
+
+  emitMessage() {
+    this.counterChange.emit(this.counterValue);
   }
 
 }
